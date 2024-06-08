83888ca3ea620c6f5e5c30e7c02033a5aaf569bc
karasb1 <bohdankaras456@gmail.com>
2024-06-08 17:05:50 +0300

    The ex1 was done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void m(double* a, double* b)
+{
+	cout << ((*a > *b) ? "number1 > number2." : "number1 < number2.");
+}
+
 int main()
 {
-
+	double n1, n2;
+	cout << "Enter the first number: ";
+	cin >> n1;
+	cout << "Enter the second number: ";
+	cin >> n2;
+	double* number1 = &n1;
+	double* number2 = &n2;
+	m(number1, number2);
 	return 0;
 }
