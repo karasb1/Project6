f07839344630ac5048035c05605283d7e80632ec
karasb1 <bohdankaras456@gmail.com>
2024-06-08 17:09:53 +0300

    The ex2 was done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void m(double* a)
+{
+	cout << ((*a > 0) ? "number +." : (*a < 0) ? "number -." : "number 0.");
+}
+
 int main()
 {
-
+	double n;
+	cout << "Enter the number: ";
+	cin >> n;
+	double* number = &n;
+	m(number);
 	return 0;
 }
